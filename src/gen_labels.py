3621eafa9eaaa2e5b0f227bc4bfca2fa767cb268
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-01-23 05:59:34 +0000

    Updated label generation

--- src/gen_labels.py
+++ src/gen_labels.py
@@ -7,13 +7,48 @@ def mkdirs(d):
     if not osp.exists(d):
         os.makedirs(d)
 
-
-def main(seq_root, label_root, seq_label="img1", gt_label="gt"):
+def gen_labels_15(seq_root, label_root, seq_label="img1", gt_label="gt"):
     seqs = [s for s in os.listdir(seq_root)]
 
     tid_curr = 0
     tid_last = -1
     for seq in seqs:
+        print(seq)
+        seq_info = open(osp.join(seq_root, seq, 'seqinfo.ini')).read()
+        seq_width = int(seq_info[seq_info.find('imWidth=') + 8:seq_info.find('\nimHeight')])
+        seq_height = int(seq_info[seq_info.find('imHeight=') + 9:seq_info.find('\nimExt')])
+
+        gt_txt = osp.join(seq_root, seq, 'gt', 'gt.txt')
+        gt = np.loadtxt(gt_txt, dtype=np.float64, delimiter=',')
+        idx = np.lexsort(gt.T[:2, :])
+        gt = gt[idx, :]
+
+        seq_label_root = osp.join(label_root, seq, 'img1')
+        mkdirs(seq_label_root)
+
+        for fid, tid, x, y, w, h, mark, _, _, _ in gt:
+            if mark == 0:
+                continue
+            fid = int(fid)
+            tid = int(tid)
+            if not tid == tid_last:
+                tid_curr += 1
+                tid_last = tid
+            x += w / 2
+            y += h / 2
+            label_fpath = osp.join(seq_label_root, '{:06d}.txt'.format(fid))
+            label_str = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
+                tid_curr, x / seq_width, y / seq_height, w / seq_width, h / seq_height)
+            with open(label_fpath, 'a') as f:
+                f.write(label_str)
+
+def gen_labels(seq_root, label_root, seq_label="img1", gt_label="gt"):
+    seqs = [s for s in os.listdir(seq_root)]
+
+    tid_curr = 0
+    tid_last = -1
+    for seq in seqs:
+        print(seq)
         seq_info = open(osp.join(seq_root, seq, 'seqinfo.ini')).read()
         seq_width = int(seq_info[seq_info.find(
             'imWidth=') + 8:seq_info.find('\nimHeight')])
@@ -26,6 +61,7 @@ def main(seq_root, label_root, seq_label="img1", gt_label="gt"):
         seq_label_root = osp.join(label_root, seq, seq_label)
         mkdirs(seq_label_root)
 
+        
         for fid, tid, x, y, w, h, mark, label, _ in gt:
             if mark == 0 or not label == 1:
                 continue
@@ -49,8 +85,11 @@ if __name__ == "__main__":
         print("\n", d)
         seq_root = f'data/MOT/{d}/images/train'
         label_root = f'data/MOT/{d}/labels_with_ids/train'
-        if not osp.exists(seq_root):
-            print(f"{seq_root} not found!")
+        if not osp.exists(seq_root) | osp.exists(label_root):
+            print(f"{seq_root} not found or {label_root} exists!")
             continue
         mkdirs(label_root)
-        main(seq_root, label_root)
+        if d == "MOT15":
+            gen_labels_15(seq_root, label_root)
+        else:
+            gen_labels(seq_root, label_root)
