be6bc434abe1f7eb5bcb73f2c3ebfeb7877532d8
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-01-23 20:45:00 +0000

    Script for embedding

---
+++ experiments/mot16_rnnforecast34_embedding.sh
@@ -0,0 +1,7 @@
+cd src
+
+python train.py mot --load_model /home/funmi/Experiments/FairMOT/models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot16.json --batch_size 8 --gpus 0,1 --forecast --num_epochs 30 --arch rnnforecast_34 --exp_id mot16_rnnforecast34 --use_embedding
+
+python /home/funmi/Experiments/FairMOT/src/track.py mot --load_model /home/funmi/Experiments/FairMOT/exp/mot/mot16_rnnforecast34/model_last.pth --conf_thres 0.6 --val_mot16 --batch_size 8 --gpu 0,1 --forecast --exp_id mot16_rnnforecast34 --arch rnnforecast_34 --use_embedding
+
+cd ..
--- experiments/mot20_dla34_rnn.sh
+++ experiments/mot20_dla34_rnn.sh
@@ -1,3 +1,3 @@
 cd src
-CUDA_VISIBLE_DEVICES='0,1' python train.py mot --exp_id forecast_rnn --load_model ../models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot20.json --batch_size 12 --gpus 0,1 --forecast --num_epochs 60
+CUDA_VISIBLE_DEVICES='0,1' python train.py mot --exp_id forecast_rnn_16 --load_model ../models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot16.json --batch_size 12 --gpus 0,1 --forecast --num_epochs 60
 cd ..
