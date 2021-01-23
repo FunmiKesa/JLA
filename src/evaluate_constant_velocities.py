65c73cb2a1f0bb1a363483f1b61cdd6ca40b41ca
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-01-23 05:07:36 +0000

    Evaluate constant velocities for all datasets

---
+++ src/evaluate_constant_velocities.py
@@ -0,0 +1,14 @@
+# import pstats
+# p = pstats.Stats('src/output.txt')
+# p.strip_dirs().sort_stats('time').print_stats(100)
+import _init_paths
+import os.path as osp
+from forecast_utils.evaluation import *
+if __name__ == "__main__":
+    datasets = ["MOT15", "MO16", "MOT20"]
+    for d in datasets:
+        label_root = f'/media2/funmi/MOT/{d}/labels_with_ids/train'
+        filename = f'/media2/funmi/MOT/{d}/images/results/forecasts_cv_10.csv'
+        if not osp.exists(label_root) | osp.exists(filename):
+            continue
+        eval(label_root, pred_folder='cv_10', pred_length=30, filename=filename)
