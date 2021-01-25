ccb3e2a5224bc0d76300e5395d73138ea13ae27f
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-01-25 14:57:47 +0000

    Formatted files

--- experiments/mot16_rnnforecast34.sh
+++ experiments/mot16_rnnforecast34.sh
@@ -1,5 +1,34 @@
 cd src
 
-python train.py mot --load_model /home/funmi/Experiments/FairMOT/models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot16.json --batch_size 8 --gpus 0,1 --forecast --num_epochs 30 --arch rnnforecast_34 --exp_id mot16_rnnforecast34 
+python train.py mot \
+--exp_id 'mot16_rnnforecast34' \
+--arch 'rnnforecast_34' \
+--load_model '../models/ctdet_coco_dla_2x.pth '\
+--data_cfg '../src/lib/cfg/mot16.json' \
+--batch_size '8' \
+--gpus '0,1' \
+--num_epochs '30' \
+--forecast \
+
+python track.py mot \
+--exp_id 'mot16_rnnforecast34' \
+--arch 'rnnforecast_34' \
+--load_model '../exp/mot/mot16_rnnforecast34/model_last.pth' \
+--conf_thres '0.6' \
+--batch_size '8' \
+--gpu '0,1' \
+--forecast \
+--val_mot16 \
+
+
+python track.py mot \
+--exp_id 'mot16_rnnforecast34' \
+--arch 'rnnforecast_34' \
+--load_model '../exp/mot/mot16_rnnforecast34/model_last.pth' \
+--conf_thres '0.6' \
+--batch_size '8' \
+--gpu '0,1' \
+--forecast \
+--val_mot20 \
 
 cd ..
--- experiments/mot16_rnnforecast34_embedding.sh
+++ experiments/mot16_rnnforecast34_embedding.sh
@@ -1,7 +1,39 @@
 cd src
 
-python train.py mot --load_model /home/funmi/Experiments/FairMOT/models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot16.json --batch_size 8 --gpus 0,1 --forecast --num_epochs 30 --arch rnnforecast_34 --exp_id mot16_rnnforecast34_embedding --use_embedding
+python train.py mot \
+--exp_id 'mot16_rnnforecast34_embedding_60' \
+--arch 'rnnforecast_34' \
+--load_model '../models/ctdet_coco_dla_2x.pth' \
+--data_cfg '../src/lib/cfg/mot16.json' \
+--batch_size '8' \
+--gpus '0,1' \
+--num_epochs '30' \
+--future_length '60' \
+--forecast \
+--use_embedding
 
-python /home/funmi/Experiments/FairMOT/src/track.py mot --load_model /home/funmi/Experiments/FairMOT/exp/mot/mot16_rnnforecast34_embedding/model_last.pth --conf_thres 0.6 --val_mot16 --batch_size 8 --gpu 0,1 --forecast --exp_id mot16_rnnforecast34_embedding --arch rnnforecast_34 --use_embedding
+python track.py mot \
+--exp_id 'mot16_rnnforecast34_embedding_60' \
+--arch 'rnnforecast_34' \
+--load_model '../exp/mot/mot16_rnnforecast34_embedding_60/model_last.pth'
+--conf_thres '0.6' \
+--batch_size '8' \
+--gpu '0,1' \
+--future_length '60' \
+--val_mot16 \
+--forecast \
+--use_embedding
+
+python track.py mot \
+--exp_id 'mot16_rnnforecast34_embedding_60' \
+--arch 'rnnforecast_34' \
+--load_model '../exp/mot/mot16_rnnforecast34_embedding_60/model_last.pth' \
+--conf_thres '0.6' \
+--batch_size '8' \
+--gpu '0,1' \
+--future_length '60' \
+--val_mot20 \
+--forecast \
+--use_embedding
 
 cd ..
