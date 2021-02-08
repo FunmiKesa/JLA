8fda2158069e2226fbb1ab3e6f966ed01d99bd4a
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-02-08 02:44:20 +0000

    Mot17 training and testing script

--- experiments/mot17_rnnforecast34.sh
+++ experiments/mot17_rnnforecast34.sh
@@ -25,43 +25,76 @@ cd src
 # --val_mot17 
 
 python train.py mot \
---exp_id 'mot17_rf34_emb_p_10_f_60_aug' \
+--exp_id 'optim_mot_17_seed' \
 --arch 'rnnforecast_34' \
 --load_model '../models/ctdet_coco_dla_2x.pth' \
 --data_cfg '../src/lib/cfg/mot17.json' \
---batch_size '8' \
---gpus '0,1' \
---num_epochs '30' \
---past_length '10' \
---future_length '60' \
+--batch_size '16' \
+--num_workers '16' \
+--gpus '0,1,2,3' \
+--num_epochs '60' \
 --forecast \
---use_embedding 
+--use_embedding \
+--multiprocessing_distributed \
+--dist-url tcp://127.0.0.1:5565 \
+--dist-backend 'nccl' \
+--world-size '1' \
+--rank '0' 
+
+# python train.py mot \
+# --exp_id 'optim_mot_17_seed' \
+# --arch 'rnnforecast_34' \
+# --load_model '../exp/mot/optim_mot_17_seed/model_last.pth' \
+# --data_cfg '../src/lib/cfg/mot17.json' \
+# --batch_size '16' \
+# --gpus '0,1,2,3' \
+# --num_workers '16' \
+# --num_epochs '31' \
+# --past_length '10' \
+# --future_length '60' \
+# --forecast \
+# --use_embedding \
+# --multiprocessing_distributed \
+# --dist-url tcp://127.0.0.1:5565 \
+# --dist-backend 'nccl' \
+# --world-size '1' \
+# --rank '0' \
+# --resume
 
 python track.py mot \
---exp_id 'mot17_rf34_emb_p_10_f_60_aug' \
+--exp_id 'optim_mot_17_seed' \
 --arch 'rnnforecast_34' \
---load_model '../exp/mot/mot17_rf34_emb_p_10_f_60_aug/model_last.pth' \
+--load_model '../exp/mot/optim_mot_17_seed/model_last.pth' \
+--conf_thres '0.6' \
+--val_mot15 \
+# --forecast \
+# --use_embedding
+
+python track.py mot \
+--exp_id 'optim_mot_17_seed' \
+--arch 'rnnforecast_34' \
+--load_model '../exp/mot/optim_mot_17_seed/model_last.pth' \
 --conf_thres '0.4' \
---forecast \
 --val_mot17 \
---use_embedding
+# --forecast \
+# --use_embedding
 
 python track.py mot \
---exp_id 'mot17_rf34_emb_p_10_f_60_aug' \
+--exp_id 'optim_mot_17_seed' \
 --arch 'rnnforecast_34' \
---load_model '../exp/mot/mot17_rf34_emb_p_10_f_60_aug/model_last.pth' \
---conf_thres '0.4' \
---forecast \
+--load_model '../exp/mot/optim_mot_17_seed/model_last.pth' \
+--conf_thres '0.3' \
 --val_mot20 \
---use_embedding
+# --forecast \
+# --use_embedding
 
-python track.py mot \
---exp_id 'test_mot17_rf34_emb_p_10_f_60_aug' \
---arch 'rnnforecast_34' \
---load_model '../exp/mot/mot17_rf34_emb_p_10_f_60_aug/model_last.pth' \
---conf_thres '0.4' \
---forecast \
---test_mot17 \
---use_embedding
+# python track.py mot \
+# --exp_id 'test_mot17_rf34_emb_p_10_f_60_aug' \
+# --arch 'rnnforecast_34' \
+# --load_model '../exp/mot/optim_mot_17_seed/model_last.pth' \
+# --conf_thres '0.4' \
+# --forecast \
+# --test_mot17 \
+# --use_embedding
 
 cd ..
