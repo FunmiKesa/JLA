55b6759033e16296381f1652e2808898f8ea81f6
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-02-19 08:36:36 +0000

    Track scripts half and full

--- experiments/track.sh
+++ experiments/track.sh
@@ -1,36 +1,25 @@
 cd src
-# python track.py mot --load_model /home/funmi/Experiments/FairMOT/exp/mot/rnnforecast_34_16/model_last.pth --conf_thres 0.6 --val_mot15 --batch_size 12 --gpu 2,3 --exp_id rnnforecast_34_16
 
-# python train.py mot --load_model /home/funmi/Experiments/FairMOT/models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot16.json --batch_size 12 --gpus 0,1 --forecast --num_epochs 30 --arch rnnforecast_34 --exp_id rnnforecast_34_mask
-
-# python train.py mot --load_model /home/funmi/Experiments/FairMOT/models/ctdet_coco_dla_2x.pth --data_cfg ../src/lib/cfg/mot16.json --batch_size 12 --gpus 0,1 --forecast --num_epochs 30 --arch rnnforecast_34 --exp_id reverse_past_bboxes
-
-# CUDA_VISIBLE_DEVICES='2' python /home/funmi/Experiments/FairMOT/src/track.py mot --load_model /home/funmi/Experiments/FairMOT/exp/mot/mot17_rf34_emb_p_10_f_60/model_last.pth --conf_thres 0.4 --val_mot17 --batch_size 8 --forecast --exp_id mot17_rf34_emb_p_10_f_60 --arch rnnforecast_34
-
-python train.py mot \
---exp_id 'optim_mot_17_seed' \
+CUDA_VISIBLE_DEVICES='1' python track_half.py mot \
+--exp_id 'mot17_half_jla34' \
 --arch 'rnnforecast_34' \
---load_model '../models/ctdet_coco_dla_2x.pth' \
---data_cfg '../src/lib/cfg/mot_sample.json' \
---batch_size '16' \
---num_workers '16' \
---gpus '0,1,2,3' \
---num_epochs '2' \
---forecast \
---use_embedding \
---multiprocessing_distributed \
---dist-url tcp://127.0.0.1:5565 \
---dist-backend 'nccl' \
---world-size '1' \
---rank '0' 
-
-CUDA_VISIBLE_DEVICES='2' python track.py mot \
---exp_id 'debug' \
---arch 'rnnforecast_34' \
---load_model '../exp/mot/optim_mot_17_seed/model_1.pth' \
+--load_model '../exp/mot/mot17_half_jla34/model_last.pth' \
 --conf_thres '0.4' \
+--past_length '10' \
+--future_length '60' \
 --val_mot17 \
---forecast \
---use_embedding
+# --forecast \
+# --use_embedding \
+
+# CUDA_VISIBLE_DEVICES='1' python track.py mot \
+# --exp_id 'mot17_half_jla34_full' \
+# --arch 'rnnforecast_34' \
+# --load_model '../exp/mot/mot17_half_jla34/model_last.pth' \
+# --conf_thres '0.4' \
+# --past_length '10' \
+# --future_length '60' \
+# --forecast \
+# --val_mot17 \
+# --use_embedding
 
 cd ..
