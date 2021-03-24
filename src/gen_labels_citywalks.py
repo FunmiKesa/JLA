b3f8e886cc35cd14c9dbafdd4c5eeaba30da61bd
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-03-24 19:47:55 +0000

    Create groundtruth files for CityWalks

--- src/gen_labels_citywalks.py
+++ src/gen_labels_citywalks.py
@@ -7,23 +7,45 @@ def mkdirs(d):
     if not osp.exists(d):
         os.makedirs(d)
 
-label_root = 'data/CityWalks/labels_with_ids'
+label_root = '../data/CityWalks/labels_with_ids'
 mkdirs(label_root)
-
-mask_rcnn = 'data/CityWalks/tracks/mask-rcnn_tracks.csv'
+is_gt = True
+label_root = '../data/CityWalks/clips'
+mask_rcnn = '../data/CityWalks/tracks/mask-rcnn_tracks.csv'
 df = pd.read_csv(mask_rcnn)
-cols =['class','track','cx','cy','w','h']
-groups = df.groupby(['vid', 'filename', 'frame_num'])
 fmt = fmt = '%d %d '+'%f ' * 4
 width = 1280
 height = 720
-for (vid, filename, fid), group in groups:
-    folder = osp.join(label_root, filename, vid.replace('.mp4', ''))
-    mkdirs(folder)
-    group['class'] = 0
-    group[['cx', 'w']] /= 1280
-    group[['cy', 'h']] /= 720
-    values = group[cols].values
+if is_gt:
+    cols =['frame_num','track','x','y','w','h','class']
+    groups = df.groupby(['vid', 'filename'])
+    fmt = fmt = '%d, %d, '+'%f, ' * 4 + ' %d'
+    for (vid, filename), group in groups:
+        print(filename, vid)
+        folder = osp.join(label_root, filename, vid.replace('.mp4', ''), 'gt')
+        mkdirs(folder)
+        group['class'] =  1
+        group['frame_num'] += 1
+        # group[['cx', 'w']] /= 1280
+        # group[['cy', 'h']] /= 720
+        group['x'] = group['cx']  - group['w'] / 2
+        group['y'] = group['cy']  - group['h'] / 2
+        
+        values = group[cols].values
 
-    label_fpath = osp.join(folder, '{:06d}.txt'.format(fid))
-    np.savetxt(label_fpath, values, delimiter=' ', fmt=fmt)
+        label_fpath = osp.join(folder, 'gt.txt')
+        np.savetxt(label_fpath, values, delimiter=',', fmt=fmt)
+else:
+    groups = df.groupby(['vid', 'filename', 'frame_num'])
+    cols =['class','track','cx','cy','w','h',]
+
+    for (vid, filename, fid), group in groups:
+        folder = osp.join(label_root, filename, vid.replace('.mp4', ''))
+        mkdirs(folder)
+        group['class'] = 0
+        group[['cx', 'w']] /= 1280
+        group[['cy', 'h']] /= 720
+        values = group[cols].values
+
+        label_fpath = osp.join(folder, '{:06d}.txt'.format(fid)) 
+        np.savetxt(label_fpath, values, delimiter=' ', fmt=fmt)
