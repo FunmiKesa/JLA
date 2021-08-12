ee0b5acd82edea25375432ab543af495598f1a77
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-08-12 11:04:04 +0100

    Updated file

--- experiments/mot17_half_jla_10_60_no_emb.sh
+++ experiments/mot17_half_jla_10_60_no_emb.sh
@@ -6,9 +6,9 @@ CUDA_VISIBLE_DEVICES='0,1' python train.py mot \
 --load_model '../models/ctdet_coco_dla_2x.pth' \
 --data_cfg '../src/lib/cfg/mot17_half.json' \
 --forecast \
---batch_size '16' \
+--batch_size '8' \
 --num_epochs '30' \
---num_workers '8' \
+--num_workers '4' \
 --gpus '0,1' \
 --past_length '10' \
 --future_length '60' \
