4641b3b5e5abb382a9977bceb5e99244d93a9d53
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-10-20 15:29:40 +0100

    Train on MOT20

--- experiments/mot20_ft_jla34_10_60.sh
+++ experiments/mot20_ft_jla34_10_60.sh
@@ -1,6 +1,6 @@
 cd src
 CUDA_VISIBLE_DEVICES='0,1' python train.py mot \
---exp_id 'mot20_ft_jla34_10_60' \
+--exp_id 'mot20_ajla34_10_60' \
 --arch 'rnnforecast_34' \
 --load_model '../jla_models/jla.pth' \
 --data_cfg '../src/lib/cfg/mot20.json' \
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES='0,1' python train.py mot \
 --future_length '60' \
 --forecast \
 --use_embedding \
---save_all
+# --save_all
 # --multiprocessing_distributed \
 # --dist-url tcp://127.0.0.1:5564 \
 # --dist-backend 'nccl' \
@@ -21,7 +21,4 @@ CUDA_VISIBLE_DEVICES='0,1' python train.py mot \
 # --rank '0' \
 
 
-python track.py mot --load_model ../exp/mot/mot20_ft_jla34_10_60/model_20.pth --conf_thres 0.3 --val_mot20 --exp_id mot20_ft_jla34_10_60 --arch rnnforecast_34 --forecast --use_embedding --no_kf --past_length 10 --future_length 60
-
-
-python track.py mot --load_model ../exp/mot/mot17_ft_ch_jla_10_60/model_30.pth --conf_thres 0.4 --val_mot17 --exp_id mot17_ft_ch_jla_10_60 --arch rnnforecast_34 --forecast --use_embedding --no_kf --past_length 10 --future_length 60
+python track.py mot --load_model ../exp/mot/mot20_ft_jla_10_60/model_20.pth --conf_thres 0.3 --val_mot20 --exp_id mot20_ft_jla_10_60 --arch rnnforecast_34 --forecast --use_embedding --no_kf --past_length 10 --future_length 60
