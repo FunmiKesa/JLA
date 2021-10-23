ce237e7b017830305b3828d4edeee194b8233c4b
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2021-10-23 21:28:36 +0100

    testNonEncodedDLAfeatures2

--- experiments/mot17_half_jla_10_60.sh
+++ experiments/mot17_half_jla_10_60.sh
@@ -1,18 +1,19 @@
 cd src
 CUDA_VISIBLE_DEVICES='0,1' python train.py mot \
---exp_id 'testNonEncodedDLAfeatures' \
---desc 'Goal: test dla features without reversing the past input' \
+--exp_id 'testNonEncodedDLAfeatures2' \
+--desc 'Goal: test dla features without reversing the past input. resume training' \
 --arch 'rnnforecast_34' \
---load_model '../models/ctdet_coco_dla_2x.pth' \
+--load_model '/home/funmi/Experiments/FairMOT2/exp/mot/testNonEncodedDLAfeatures2/model_last.pth' \
 --data_cfg '../src/lib/cfg/mot17_half.json' \
 --batch_size '8' \
---num_epochs '30' \
---num_workers '8' \
+--num_epochs '60' \
+--num_workers '4' \
 --gpus '0,1' \
 --past_length '10' \
 --future_length '60' \
 --forecast \
 --use_embedding \
+--resume
 # --multiprocessing_distributed \
 # --dist-url tcp://127.0.0.1:5564 \
 # --dist-backend 'nccl' \
@@ -20,5 +21,5 @@ CUDA_VISIBLE_DEVICES='0,1' python train.py mot \
 # --rank '0' \
 
 
-python track_half.py mot --load_model ../exp/mot/testNonEncodedDLAfeatures/model_last.pth --conf_thres 0.4 --val_mot17 --exp_id testNonEncodedDLAfeatures --arch rnnforecast_34 --forecast --use_embedding --no_kf --past_length 10 --future_length 60
+python track_half.py mot --load_model ../exp/mot/testNonEncodedDLAfeatures2/model_last.pth --conf_thres 0.4 --val_mot17 --exp_id testNonEncodedDLAfeatures2 --arch rnnforecast_34 --forecast --use_embedding --no_kf --past_length 10 --future_length 60
 
